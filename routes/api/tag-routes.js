e3e79a96612f78ec5d426eaa3d773d87f7983b7f
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-08 13:40:39 -0500

    adding rout

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,11 +5,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+    include: [
+      {
+        model: Product
+      }
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Product data
 });
 
